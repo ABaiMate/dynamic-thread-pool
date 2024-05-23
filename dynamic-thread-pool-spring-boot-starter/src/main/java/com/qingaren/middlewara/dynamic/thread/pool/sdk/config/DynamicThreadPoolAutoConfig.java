28350ea5a4b3febd47250519cf6ddff81cfc32a7
QingAren <2401097763@qq.com>
2024-05-23 18:11:00 +0800

    feat：搭建组件工程

---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/qingaren/middlewara/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -0,0 +1,12 @@
+package com.qingaren.middlewara.dynamic.thread.pool.sdk.config;
+
+/**
+ * @projectName: dynamic-thread-pool
+ * @author: QingAren
+ * @description: 动态配置入口
+ * @github https://https://github.com/ABaiMate
+ * @date: 2024-05-23 18:07
+ */
+
+public class DynamicThreadPoolAutoConfig {
+}
---
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/qingaren/middlewara/dynamic/thread/pool/sdk/registry/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @param null:
+  * @return null
+ * @author QingAren
+ * @description 注册中心
+ * @date 2024-05-23 16:58
+ */
+package com.qingaren.middlewara.dynamic.thread.pool.sdk.registry;
